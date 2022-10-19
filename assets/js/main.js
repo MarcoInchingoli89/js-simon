74abdf858a07024cfefdac06bf905d858e36ee1f
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-19 15:21:28 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/* Consegna */
+
+/*Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+/* Strumenti */
+//const e let
+//querySelector
+//createElement
+//className
+//append
+//math.random - math.floor
+//innerText
+//insertAdjacentHTML o append
+
+/* Istruzioni */
+//Visualizzare in pagina 5 numeri casuali
+
+//Creo in html un container
+//Seleziono il container e lo aggiungo ad una variabile
+//Creo in js un div e lo aggiungo ad una variabile
+//Assegno la classe "numbers" al div
+//Lo inserisco nel container
+//Creo i 5 numeri casuali e li salvo in una variabile
+//Seleziono la classe "numbers"
+//Inserisco i numeri all'interno di "numbers"
+//Stampo l'elemento con i numeri in pagina
+
