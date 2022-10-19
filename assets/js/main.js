8fb30c25a78666b92d15686b5a146be38f29f271
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-19 15:43:31 +0200

    Add class numbers, randomnumbers and add to page

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,24 @@
 
 //Creo in html un container
 //Seleziono il container e lo aggiungo ad una variabile
+const simonContainer = document.querySelector('.container');
+/* console.log(simonContainer); */
 //Creo in js un div e lo aggiungo ad una variabile
+const divElement = document.createElement('div');
+/* console.log(numberElement); */
 //Assegno la classe "numbers" al div
+divElement.className = ('numbers');
+/* console.log(numberElement); */
 //Lo inserisco nel container
+simonContainer.append(divElement);
 //Creo i 5 numeri casuali e li salvo in una variabile
+const randomNumbers = Math.floor(Math.random() * 100);
+/* console.log(randomNumbers); */
 //Seleziono la classe "numbers"
+const numbersElement = document.querySelector('.numbers');
+/* console.log(numbersElement); */
 //Inserisco i numeri all'interno di "numbers"
+numbersElement.innerText = randomNumbers;
+console.log(numbersElement)
 //Stampo l'elemento con i numeri in pagina
 
